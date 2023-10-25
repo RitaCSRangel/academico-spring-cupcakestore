996df771ac8f01c1ac8f75b152fc2b159e1f3512
Rita Rangel <rita.d.rangel@accenture.com>
2023-10-25 12:44:55 -0300

    fix: update CORS policy

--- src/main/java/com/cupcakestore/store/CupcakeStoreApplication.java
+++ src/main/java/com/cupcakestore/store/CupcakeStoreApplication.java
@@ -21,19 +21,19 @@ public class CupcakeStoreApplication {
 
 	// Function: CorsFilter
 	// Função responsável por permitir que a aplicação estabeleça suas chamadas de API sem ser interrompida pelo CORS
-	// @Bean
-	// public CorsFilter corsFilter() {
-	// 	CorsConfiguration corsConfiguration = new CorsConfiguration();
-	// 	corsConfiguration.setAllowCredentials(true);
-	// 	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-	// 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-	// 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-	// 			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-	// 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-	// 			"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-	// 	corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-	// 	UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-	// 	urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-	// 	return new CorsFilter(urlBasedCorsConfigurationSource);
-	// }
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://rita-cupcakestore-site-c4b28748e517.herokuapp.com"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
